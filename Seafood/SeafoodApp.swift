fd0291f2f1a51847f28b947649c45dc1df3947e0
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2023-01-13 19:33:22 -0300

    Initial Commit

---
+++ Seafood/SeafoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeafoodApp.swift
+//  Seafood
+//
+//  Created by Lucas Migge de Barros on 13/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SeafoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
